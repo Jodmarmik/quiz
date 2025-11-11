4ad3f46f0967437aa4db14b88d78b5f901abd247
Howtog41 <howtog41@gmail.com>
2025-11-11 20:46:08 +0530

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = '8266320488:AAEWtfJWtOFeSnC5zs9wnK6gSHDoO9Gq76g'
-ADMIN_ID = 5018200809
-MONGO_URI = 'mongodb+srv://latestkoreandraama:P5jRORGqeJsf9nDJ@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
